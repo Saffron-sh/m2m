9a1927677387fb529010a47ba823e512c1ba02ad
saffron <qwertysaffron@tutamail.com>
2025-05-24 04:25:57 +0530

    A less verbose version of m2m, works without printing much on the rerminal.

--- m2m.sh
+++ m2m.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
-yt-dlp $1 -o $YTDIR/ytvideo.webm
-ffmpeg -i $YTDIR/ytvideo.web* $YTDIR/$2
+echo "No need to rage quit, m2m is working."
+yt-dlp $1 -q -o $YTDIR/ytvideo.webm
+echo "Done downloading. Now converting"
+ffmpeg -v 8 -i $YTDIR/ytvideo.web* $YTDIR/$2
 rm $YTDIR/ytvideo.web*
+echo "All done, go check out your file in $YTDIR"
